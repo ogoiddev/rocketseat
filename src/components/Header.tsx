7afb80a9167067e150cc1909b830d45fe9428f4d
ogoiddev <diogompantaleao@gmail.com>
2022-06-23 04:20:47 -0300

    update

--- src/components/Header.tsx
+++
@@ -1,9 +0,0 @@
-import { Logo } from './Logo';
-
-export default function Header() {
-  return (
-    <header className="w-full py-5 flex items-center justify-center bg-gray-700 border-b border-gray-600">
-      <Logo/>
-    </header>
-  )
-}
--- src/lib/apollo.ts
+++
@@ -1,9 +0,0 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
-
-export const client = new ApolloClient({
-  uri: 'https://api-sa-east-1.graphcms.com/v2/cl4nlk81z0gjq01xmb4ix8qct/master',
-  cache: new InMemoryCache()
-});
-
-// copiado do video // 
-// uri: 'https://api-sa-east-1.graphcms.com/v2/cl4ljcdt56mom01yrdswgf7n9/master',

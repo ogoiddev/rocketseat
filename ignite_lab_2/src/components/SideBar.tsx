ccc73ef107bd77dcd15e9893f4a2b0d828994b53
ogoiddev <diogompantaleao@gmail.com>
2022-06-26 00:22:48 -0300

    feat: day 5 done. codegen defined and all types imported auto

--- ignite_lab_2/src/components/SideBar.tsx
+++ ignite_lab_2/src/components/SideBar.tsx
@@ -1,31 +1,10 @@
 import { gql, useQuery } from "@apollo/client";
+import { useGetLessonsQuery } from "../graphql/generated";
 import Lesson from "./Lesson";
 
 
-const GET_LESSON_QUERY = gql`
-    query MyQuery {
-    lessons(orderBy: availableAt_ASC, stage: PUBLISHED) {
-      id
-      lessonType
-      availableAt
-      slug
-      title
-    }
-  }
-`
-
-interface GetLessonsQueryResponse {
-  lessons: {
-    id: string;
-    title: string;
-    slug: string;
-    availableAt: string;
-    lessonType: 'live' | 'class';
-  }[]
-}
-
 export default function SideBar() {
-  const { data } = useQuery<GetLessonsQueryResponse>(GET_LESSON_QUERY)
+  const { data } = useGetLessonsQuery()
 
   return (
     <aside className="w-[348px] bg-gray-700 p-6 border-1"> 
@@ -48,3 +27,57 @@ export default function SideBar() {
     </aside >
   )
 }
+
+
+
+
+// import { gql, useQuery } from "@apollo/client";
+// import Lesson from "./Lesson";
+
+
+// const GET_LESSON_QUERY = gql`
+//     query MyQuery {
+//     lessons(orderBy: availableAt_ASC, stage: PUBLISHED) {
+//       id
+//       lessonType
+//       availableAt
+//       slug
+//       title
+//     }
+//   }
+// `
+
+// interface GetLessonsQueryResponse {
+//   lessons: {
+//     id: string;
+//     title: string;
+//     slug: string;
+//     availableAt: string;
+//     lessonType: 'live' | 'class';
+//   }[]
+// }
+
+// export default function SideBar() {
+//   const { data } = useQuery<GetLessonsQueryResponse>(GET_LESSON_QUERY)
+
+//   return (
+//     <aside className="w-[348px] bg-gray-700 p-6 border-1"> 
+//       <span className="font-bold text-2x1 pb-6 mb-6 border-b border-gray-500 block">
+//         Cronograma das aulas
+//       </span>
+
+//       <div className="flex flex-col gap-8">
+//         {data?.lessons.map(lesson => 
+//             <Lesson
+//               key={lesson.id}
+//               title={lesson.title}
+//               slug={lesson.slug}
+//               availableAt={new Date(lesson.availableAt)}
+//               type={lesson.lessonType}
+//             />
+//         )}
+//       </div>
+
+//     </aside >
+//   )
+// }
